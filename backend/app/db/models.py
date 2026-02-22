44aa683fffaafbcf6b33afd8ffdca06b62a83921
Babali Shirodkar <aniketshirodkar02@gmail.com>
2026-02-22 16:56:32 +0530

    working login api and prevent umautharized access api

---
+++ backend/app/db/models.py
@@ -0,0 +1,13 @@
+from app import db
+import uuid
+
+
+class User(db.Model):
+    __tablename__ = "users"
+
+    user_id = db.Column(db.UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    role_id = db.Column(db.UUID(as_uuid=True), nullable=False)
+    name = db.Column(db.Text, nullable=False)
+    email = db.Column(db.Text, unique=True, nullable=False)
+    password_hash = db.Column(db.Text, nullable=False)
+    status = db.Column(db.Boolean, default=True)
---
+++ backend/app/routes/auth_routes.py
@@ -0,0 +1,67 @@
+from flask import Blueprint, request, jsonify
+from werkzeug.security import check_password_hash
+from app.db.models import User
+from flask_jwt_extended import create_access_token
+from app import db   
+from flask_jwt_extended import jwt_required, get_jwt_identity
+
+auth_bp = Blueprint("auth", __name__)
+
+# testing connection
+@auth_bp.route("/test")
+def test_route():
+    return jsonify({"message": "Auth Route Working"})
+
+# login api 
+@auth_bp.route("/login", methods=["POST"])
+def login():
+
+    data = request.get_json()
+
+    email = data.get("email")
+    password = data.get("password")
+
+    if not email or not password:
+        return jsonify({"message": "Email and password required"}), 400
+
+    user = User.query.filter_by(email=email, status=True).first()
+
+    if not user:
+        return jsonify({"message": "Invalid credentials"}), 401
+
+    if not check_password_hash(user.password_hash, password):
+        return jsonify({"message": "Invalid credentials"}), 401
+
+    access_token = create_access_token(
+        identity=str(user.user_id),
+        additional_claims={
+            "role_id": str(user.role_id)
+        }
+    )
+
+
+    return jsonify({
+        "token": access_token,
+        "role": str(user.role_id),
+        "user_id": str(user.user_id),
+        "name": user.name
+    }), 200
+
+
+#preventing unautharized access
+
+from flask_jwt_extended import jwt_required, get_jwt_identity, get_jwt
+
+@auth_bp.route("/protected", methods=["GET"])
+@jwt_required()
+def protected():
+    user_id = get_jwt_identity()   # from identity
+    claims = get_jwt()             # full token data
+    
+    role_id = claims.get("role_id")
+
+    return jsonify({
+        "message": "Access granted",
+        "user_id": user_id,
+        "role_id": role_id
+    }), 200
