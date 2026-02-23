05fecd1c1b9649b220014221eba4d5e380877aa5
Babali Shirodkar <aniketshirodkar02@gmail.com>
2026-02-23 14:37:25 +0530

    added dashboard route

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -4,6 +4,7 @@ from app.core.config import Config
 from app.db.database import db
 from flask_jwt_extended import JWTManager
 from app.routes.auth_routes import auth_bp
+from app.routes.dashboard_routes import dashboard_bp
 def create_app():
     app = Flask(__name__)
 
@@ -13,7 +14,8 @@ def create_app():
     db.init_app(app)
     JWTManager(app)
 
-    app.register_blueprint(auth_bp, url_prefix="/api/auth")
+    app.register_blueprint(auth_bp, url_prefix="/api/auth"),
+    app.register_blueprint(dashboard_bp, url_prefix="/api/dashboard")
 
     print("SECRET_KEY:", app.config["SECRET_KEY"])
     return app
